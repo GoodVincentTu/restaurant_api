5d70a4b1c458fa92ec8cde89a9daba157f8db992
metatrontu@gmail.com <metatrontu@gmail.com>
2016-07-08 23:34:08 +0800

    finish adding items to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,6 +18,20 @@ class OrdersController < ApplicationController
     end
   end
 
+  def add
+    @order = Order.find params[:id]
+    # order_item = @order.order_items.build(item_id: params[:item_id])
+
+    order_item = OrderItem.where(order_id: @order.id, item_id: params[:item_id]).first.increment(:quantity) rescue
+      @order.order_items.build(item_id: params[:item_id])
+
+    if order_item.save
+      render json: order_item, status: :created #201
+    else
+      render json: order_item.errors, status: :unprocessable_entity #422
+    end
+  end
+
   private
     # Only allow a trusted parameter "white list" through.
     def order_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :orders, only: [:index] #/orders
 
   resources :tables, except: [:new, :edit] do
-  	resources :orders, only: [:create] #/tables/:id/orders
+  	resources :orders, only: [:create] do #/tables/:id/orders
+      post :add, on: :member
+  	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
