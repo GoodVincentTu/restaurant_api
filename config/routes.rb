5ed64ac4d4ba81cbfd15cd39bfd8632c3e482ebc
metatrontu@gmail.com <metatrontu@gmail.com>
2016-07-08 18:01:43 +0800

    add orders feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :tables, except: [:new, :edit]
+  resources :orders, only: [:index] #/orders
+
+  resources :tables, except: [:new, :edit] do
+  	resources :orders, only: [:create] #/tables/:id/orders
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
